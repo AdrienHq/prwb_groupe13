e9b70e11a8c33f4ca034e3df2f6fdc30b8099fd1
AdrienHq <adrienhecq@gmail.com>
2022-05-09 20:43:19 +0200

    feat: footer

--- view/view_base.php
+++ view/view_base.php
@@ -5,6 +5,7 @@
         <title>Connectez vous !</title>
         <base href="<?= $web_root ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/font-awesome.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
@@ -20,14 +21,17 @@
         <header>
             <?php require_once("view_navbar.html"); ?>
             <?php require_once("view_header.html"); ?>
-            <?php require_once("view_footer.html"); ?>
         </header>
         <section>
             <div class="container">
                 <div class="row">
-
                 </div>
             </div>
         </section>
+        <footer class="footer" id="footer">
+            <div class="container">
+                <?php require_once("view_footer.html"); ?>
+            </div>
+        </footer>
     </body>
 </html>
