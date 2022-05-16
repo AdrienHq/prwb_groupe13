5d27332a020377e7380df8c9b4de3668ce1a44d3
AdrienHq <adrienhecq@gmail.com>
2022-05-16 20:12:53 +0200

    refactor

--- view/view_index.php
+++ view/view_index.php
@@ -42,14 +42,4 @@
             <?php require_once("view/blocks/view_footer.html"); ?>
         </div>
     </footer>
-<!--    <body id="profil" >-->
-<!--        <div class="title">Bienvenue sur Projet13</div>-->
-<!--        <div class="menu">-->
-<!--            <a href="login/login">Connection</a>-->
-<!--            <a href="login/signup">S'enregistrer</a>-->
-<!--        </div>-->
-<!--        <div class="main">-->
-<!--            Enregistrez ou connectez-vous.-->
-<!--        </div>-->
-<!--    </body>-->
 </html>
