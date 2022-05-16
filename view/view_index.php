6aa93623d568b5b47a5fc476048a9d444a2dd419
AdrienHq <adrienhecq@gmail.com>
2022-05-16 20:22:40 +0200

    feat: landing page

--- view/view_index.php
+++ view/view_index.php
@@ -6,36 +6,32 @@
         <?php require_once("view/blocks/view_header.html"); ?>
     </header>
     <body>
-        <main role="main">
+        <section role="main">
             <div class="baseline">
                 <span class="baseline-welcome">Gimme</span>
                 <span class="baseline-slogan">Your MONEY</span>
                 <span class="baseline-descr">Will be better in my pocket</span>
             </div>
-
-
-            <div class="container">
-                <!-- Example row of columns -->
+            <div class="container" style="text-align: center">
                 <div class="row">
                     <div class="col-md-4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <h2 style="background-color: #666666; margin-top: 15px; padding: 5px 5px 15px">Our story</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt dicta illo molestias praesentium quas repudiandae similique sunt unde vel!</p>
                         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
                     </div>
                     <div class="col-md-4">
-                        <h2>Heading</h2>
-                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <h2 style="background-color: #666666; margin-top: 15px; padding: 5px 5px 15px">Our engagement</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt dicta illo molestias praesentium quas repudiandae similique sunt unde vel!</p>
                         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
                     </div>
                     <div class="col-md-4">
-                        <h2>Heading</h2>
-                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                        <h2 style="background-color: #666666; margin-top: 15px; padding: 5px 5px 15px">Yes we ripped you off</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt dicta illo molestias praesentium quas repudiandae similique sunt unde vel!</p>
                         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
                     </div>
                 </div>
-                <hr>
             </div>
-    </main>
+        </section>
     </body>
     <footer class="footer" id="footer">
         <div class="container">
