a231706daf70c4126d7e4e078b26bd747a5217e3
AdrienHq <adrienhecq@gmail.com>
2022-05-16 18:06:46 +0200

    feat: Jumbotron init

--- view/view_index.php
+++ view/view_index.php
@@ -1,14 +1,55 @@
 <!DOCTYPE html>
 <html>
     <?php require_once("view/blocks/head.html"); ?>
-    <body id="profil" >
-        <div class="title">Bienvenue sur Projet13</div>
-        <div class="menu">
-            <a href="login/login">Connection</a>
-            <a href="login/signup">S'enregistrer</a>
-        </div>
-        <div class="main">
-            Enregistrez ou connectez-vous.
-        </div>
+    <header>
+        <?php require_once("view/blocks/view_navbar.html"); ?>
+        <?php require_once("view/blocks/view_header.html"); ?>
+    </header>
+    <body>
+        <main role="main">
+            <div class="baseline">
+                <span class="baseline-welcome">Gimme</span>
+                <span class="baseline-slogan">Your MONEY</span>
+                <span class="baseline-descr">Will be better in my pocket</span>
+            </div>
+
+
+            <div class="container">
+                <!-- Example row of columns -->
+                <div class="row">
+                    <div class="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                    </div>
+                    <div class="col-md-4">
+                        <h2>Heading</h2>
+                        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                    </div>
+                </div>
+                <hr>
+            </div>
+    </main>
     </body>
+    <footer class="footer" id="footer">
+        <div class="container">
+            <?php require_once("view/blocks/view_footer.html"); ?>
+        </div>
+    </footer>
+<!--    <body id="profil" >-->
+<!--        <div class="title">Bienvenue sur Projet13</div>-->
+<!--        <div class="menu">-->
+<!--            <a href="login/login">Connection</a>-->
+<!--            <a href="login/signup">S'enregistrer</a>-->
+<!--        </div>-->
+<!--        <div class="main">-->
+<!--            Enregistrez ou connectez-vous.-->
+<!--        </div>-->
+<!--    </body>-->
 </html>
