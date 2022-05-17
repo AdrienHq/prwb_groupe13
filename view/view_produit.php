114629ec0897189bdb59f6a637aa234f444f0a2b
AdrienHq <adrienhecq@gmail.com>
2022-05-17 12:40:21 +0200

    feat: unique product card

--- view/view_produit.php
+++ view/view_produit.php
@@ -1,41 +1,45 @@
 <!DOCTYPE html>
 <html>
     <?php require_once("view/blocks/head.html"); ?>
-    <head>
-        <meta charset="UTF-8">
-        <title>Categorie TEST</title>
-        <base href="<?= $web_root ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/style1.css">
-        <script type="text/javascript" src="lib/jquery-2.2.0.min.js"></script>
-        <script type="text/javascript" src="lib/unslider/unslider-min.js"></script>
-        <script type="text/javascript" src="js/panier.js"></script>
-        <link rel="stylesheet" type="text/css" href="lib/unslider/unslider.css">
-        <link rel="stylesheet" type="text/css" href="lib/unslider/unslider-dots.css">
-    </head>
     <body>
         <?php require_once("view/blocks/view_navbar.html"); ?>
-
-        <h2>Produit : <?= $produit->label ?></h2>
-
-            <div class="banner">
-                <ul>
-                    <li><?= "<img src=".$produit->photo.' width = "400" height = "400" alt = "img"/>' ?></li>
-                    <li><?= "<img src=".$produit->photo.' width = "400" height = "400" alt = "img"/>' ?></li>
-                    <li><?= "<img src=".$produit->photo.' width = "400" height = "400" alt = "img"/>' ?></li>
-                    <li><?= "<img src=".$produit->photo.' width = "400" height = "400" alt = "img"/>' ?></li>
-                </ul>
+        <h2 style="margin-top:60px; text-align: center"><?= $produit->label ?> </h2>
+        <section style="background-color: white;">
+            <div class="container py-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-4">
+                        <div class="card text-black">
+                            <?= "<img src=".$produit->photo.' width = "100%" height = "100%" alt = "img"/>' ?>
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h5>Produit : <?= $produit->label ?></h5>
+                                    <p class="text-muted mb-4">Apple pro display XDR</p>
+                                </div>
+                                <div>
+                                    <div class="d-flex justify-content-between">
+                                        <span><strong>Label</strong></span><span><?= $produit->label ?></span>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <span><strong>Description</strong></span><span><?= $produit->descr ?></span>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <span><strong>Prix</strong></span><span><?= $produit->prix ?></span>
+                                    </div>
+                                </div>
+                                <div class="d-flex justify-content-between total font-weight-bold mt-4">
+                                    <span id="idProd" hidden value="<?= $produit->id ?>"></span>
+                                    <input type="button" class="btn btn-dark" value="Ajouter au panier" onclick="addToBasket()">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- <?= "<span><img src=".$produit->photo.' width = "400" height = "400" alt = "img"/><span>' ?> -->
-            <span class="label_profil">Label</span> : <?= $produit->label ?><br>
-            <span class="label_profil">Description</span> : <?= $produit->descr ?><br>
-            <span class="label_profil">Prix</span> : <?= $produit->prix ?><br>
-            <span id="idProd" hidden value="<?= $produit->id ?>"></span>
-            <input type="button" value="Ajouter au panier" onclick="addToBasket()">
-        <script>
-            $(function() {
-                $('.banner').unslider();
-            });
-        </script>
+        </section>
     </body>
+    <footer class="footer" id="footer">
+        <div class="container">
+            <?php require_once("view/blocks/view_footer.html"); ?>
+        </div>
+    </footer>
 </html>
