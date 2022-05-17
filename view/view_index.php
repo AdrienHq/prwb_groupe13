b298bcdb16c92186f8b64ab991b2ed332d8cf7af
AdrienHq <adrienhecq@gmail.com>
2022-05-17 12:30:58 +0200

    feat: modal bc why not

--- view/view_index.php
+++ view/view_index.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
     <?php require_once("view/blocks/head.html"); ?>
+    <script>
+        var myModal = document.getElementById('myModal')
+        var myInput = document.getElementById('myInput')
+
+        myModal.addEventListener('shown.bs.modal', function () {
+            myInput.focus()
+        })
+    </script>
     <header>
         <?php require_once("view/blocks/view_navbar.html"); ?>
         <?php require_once("view/blocks/view_header.html"); ?>
@@ -13,21 +21,75 @@
                 <span class="baseline-descr">Will be better in my pocket</span>
             </div>
             <div class="container" style="text-align: center">
-                <div class="row">
+                <div class="row" style="margin-bottom: 150px; margin-top: 50px">
                     <div class="col-md-4">
                         <h2 style="background-color: #666666; margin-top: 15px; padding: 5px 5px 15px">Our story</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt dicta illo molestias praesentium quas repudiandae similique sunt unde vel!</p>
-                        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            View Details
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Details</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores beatae cum exercitationem, illo molestias, necessitatibus nesciunt nihil odit omnis quam quas repellat, rerum. Aliquam consequatur doloremque dolorum, eligendi, eum ex facilis in laborum nostrum porro possimus quibusdam quis quo ratione unde, vel vero voluptatum! Ab pariatur placeat quis ratione!
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-md-4">
                         <h2 style="background-color: #666666; margin-top: 15px; padding: 5px 5px 15px">Our engagement</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt dicta illo molestias praesentium quas repudiandae similique sunt unde vel!</p>
-                        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            View Details
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Details</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores beatae cum exercitationem, illo molestias, necessitatibus nesciunt nihil odit omnis quam quas repellat, rerum. Aliquam consequatur doloremque dolorum, eligendi, eum ex facilis in laborum nostrum porro possimus quibusdam quis quo ratione unde, vel vero voluptatum! Ab pariatur placeat quis ratione!
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-md-4">
                         <h2 style="background-color: #666666; margin-top: 15px; padding: 5px 5px 15px">Yes we ripped you off</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium deserunt dicta illo molestias praesentium quas repudiandae similique sunt unde vel!</p>
-                        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            View Details
+                        </button>
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-scrollable">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Details</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores beatae cum exercitationem, illo molestias, necessitatibus nesciunt nihil odit omnis quam quas repellat, rerum. Aliquam consequatur doloremque dolorum, eligendi, eum ex facilis in laborum nostrum porro possimus quibusdam quis quo ratione unde, vel vero voluptatum! Ab pariatur placeat quis ratione!
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
